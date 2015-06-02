841555d6bc34b1f84515beeed13f3a521b763cd7
Victor Basso <victorbasso@sagaranatech.com>
2015-06-01 22:43:51 -0300

    flask helloworld

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import os
 
-virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+virtenv = os.path.join(os.environ.get('OPENSHIFT_PYTHON_DIR','.'), 'virtenv')
 virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
 try:
     execfile(virtualenv, dict(__file__=virtualenv))
@@ -21,4 +21,4 @@ if __name__ == '__main__':
     from wsgiref.simple_server import make_server
     httpd = make_server('localhost', 8051, application)
     # Wait for a single request, serve it and quit.
-    httpd.handle_request()
+    httpd.serve_forever()
